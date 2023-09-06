334bb64e94bdbec533d80bff7fbeaea4d8188027
rubenromaniversuni <138502323+rubenromaniversuni@users.noreply.github.com>
2023-09-06 16:02:08 +0200

    test commit

--- 6502_cpu_emulator_TST/6502StatusFlagsTest.h
+++ 6502_cpu_emulator_TST/6502StatusFlagsTest.h
@@ -28,7 +28,21 @@ public:
 
 };
 
-TEST_F(M6502StatusFlagsTest, BVSBackwardCrosspage)
+TEST_F(M6502StatusFlagsTest, CLCClearCarryFlag)
 {
-	
+	// Given:
+	cpu.Reset(mem, 0xFF00);
+	cpu.PS.Flags.C = true;
+	mem[0xFF00] = CPU::INS_CLC;
+	const s32 EXPECTED_CYCLES = 2;
+	CpuMakeCopy();
+
+	// When:
+	const s32 ActualCycles = cpu.Execute(EXPECTED_CYCLES, mem);
+
+	// Then:
+	EXPECT_EQ(ActualCycles, EXPECTED_CYCLES);
+	EXPECT_EQ(mem[0x0080 + offsetAddr], zero ? 0x00 : (negative ? 0x80 : 0x02));
+	EXPECT_EQ(cpu.PS.Flags.Z, zero ? true : false);
+	EXPECT_EQ(cpu.PS.Flags.N, negative ? true : false);
 }
